5dae1ce1137a96e9fef67580ffd3b2a804ed549c
boringbyte <kottapallisivaram@gmail.com>
2020-08-20 18:58:59 -0400

    swith statemtn

--- CalcEngine/src/com/pluralsight/calcengine/Main.java
+++ CalcEngine/src/com/pluralsight/calcengine/Main.java
@@ -3,6 +3,10 @@ package com.pluralsight.calcengine;
 public class Main {
 
     public static void main(String[] args) {
+        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+        char[] opCodes = {'d', 'a', 's', 'm'};
+        double[] results = new double[opCodes.length];
         double value1 = 100.0d;
         double value2 = 0.0d;
         double result = 0.0d;
@@ -40,25 +44,63 @@ public class Main {
 
         // switch can only be used with byte, short, int, long and char data types
         // A match can have multiple statements and each match with break. Otherwise will "fall through" to next match
-        switch (opCode) {
-            case 'a':
-                result = value1 + value2;
-                break;
-            case 's':
-                result = value1 - value2;
-                break;
-            case 'm':
-                result = value1 * value2;
-                break;
-            case 'd':
-                result = value2 != 0 ? value1 / value2 : 0.0d;
-                break;
-            default:
-                System.out.println("Invalid opCode: " + opCode);
-                result = 0.0d;
-                break;
+        for(int x = 0; x < opCodes.length; x++) {
+            switch (opCodes[x]) {
+                case 'a':
+                    results[x] = leftVals[x] + rightVals[x];
+                    break;
+                case 's':
+                    results[x] = leftVals[x] - rightVals[x];
+                    break;
+                case 'm':
+                    results[x] = leftVals[x] * rightVals[x];
+                    break;
+                case 'd':
+                    results[x] = leftVals[x] != 0 ? leftVals[x] / rightVals[x] : 0.0d;
+                    break;
+                default:
+                    System.out.println("Invalid opCode: " + opCode);
+                    result = 0.0d;
+                    break;
+            }
         }
-        System.out.println(result);
+        for (double currentResult: results) {
+            System.out.println(currentResult);
+        }
 
+        // While loop
+        int i = 10;
+        while (i >= 0) {
+            System.out.println("Current i value: " + i);
+            i--;
+        }
+
+        // Do-while loop
+        int j = 10;
+        do{
+            System.out.println("Current j value: " + j);
+            j--;
+        } while (j >= 0);
+
+        // for loop
+        for (int k=10; k>=0; k--) {
+            System.out.println("Current k value: " + k);
+        }
+
+        // arrays loop
+        float[] theVals = new float[3];
+        theVals[0] = 10.0f;
+        theVals[1] = 20.0f;
+        theVals[2] = 30.0f;
+        float sum = 0.0f;
+        for(int index=0; index < theVals.length; index++)
+            sum += theVals[index];
+        System.out.println(sum);
+
+        // for each loop
+        for (float currentVal: theVals) {
+            sum += currentVal;
+        }
+        System.out.println(sum);
     }
 }
