8d9e0aac46d7261c98a5731be596748e2f25c3a6
boringbyte <kottapallisivaram@gmail.com>
2020-08-27 08:09:40 -0400

    static members

---
+++ CalcEngine2/src/com/pluralsight/calcengine2/MathEquation.java
@@ -0,0 +1,84 @@
+package com.pluralsight.calcengine2;
+
+public class MathEquation {
+    private double leftVal;
+    private double rightVal;
+    private char opCode;
+    private double result;
+
+    public void setLeftVal(double leftVal) {
+        this.leftVal = leftVal;
+    }
+
+    public void setRightVal(double rightVal) {
+        this.rightVal = rightVal;
+    }
+
+    public void setOpCode(char opCode) {
+        this.opCode = opCode;
+    }
+
+    public void setResult(double result) {
+        this.result = result;
+    }
+
+    public double getLeftVal() {
+        return leftVal;
+    }
+
+    public double getRightVal() {
+        return rightVal;
+    }
+
+    public char getOpCode() {
+        return opCode;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    private static int numberOfCalculations;
+    private static double sumOfResults;
+
+    public static double getAverageResult() {
+        return sumOfResults / numberOfCalculations;
+    }
+
+    public MathEquation() {}
+
+    public MathEquation(char opCode) {
+        this.opCode = opCode;
+    }
+
+    public MathEquation(char opCode, double leftVal, double rightVal) {
+        this(opCode);
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+    }
+
+    void execute() {
+        switch (opCode) {
+            case 'a':
+                result = leftVal + rightVal;
+                break;
+            case 's':
+                result = leftVal - rightVal;
+                break;
+            case 'm':
+                result = leftVal * rightVal;
+                break;
+            case 'd':
+                result = leftVal != 0 ? leftVal / rightVal : 0.0d;
+                break;
+            default:
+                System.out.println("Invalid opCode: " + opCode);
+                result = 0.0d;
+                break;
+        }
+        numberOfCalculations += 1;
+        sumOfResults += result;
+    }
+
+
+}
